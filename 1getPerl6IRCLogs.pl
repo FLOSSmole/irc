a6eb26607a0e6a89ebc4bdda6196e31671a76cc3
Megan Squire <megansquire@gmail.com>
2016-06-06 10:43:05 -0400

    formatting

--- 1getPerl6IRCLogs.pl
+++ 1getPerl6IRCLogs.pl
@@ -1,9 +1,10 @@
-#!/usr/bin/perl
+# -*- coding: utf-8 -*-
 ## This program is free software; you can redistribute it
 ## and/or modify it under the same terms as Perl itself.
 ## Please see the Perl Artistic License 2.0.
 ## 
-## Copyright (C) 2004-2015 Megan Squire <msquire@elon.edu>
+## Copyright (C) 2004-2016 Megan Squire <msquire@elon.edu>
+## Other contributors: Greg Batchelor - converted perl to python
 ##
 ## We're working on this at http://flossmole.org - Come help us build 
 ## an open and accessible repository for data and analyses for open
@@ -18,22 +19,24 @@
 ##
 ## and
 ##
-## FLOSSmole (2004-2015) FLOSSmole: a project to provide academic access to data 
+## FLOSSmole (2004-2016) FLOSSmole: a project to provide academic access to data 
 ## and analyses of open source projects.  Available at http://flossmole.org 
 #
 ################################################################
 # usage:
-# > perl 1getPerl6IRCLogs.pl <new_datasource_id> <date-to-start> 
+# > python 1getPerl6IRCLogs.py <new_datasource_id> <date-to-start> <password>
 #
 # THIS DATASOURCE IS THE NEXT ONE AVAIL IN THE DB - AND IT WILL GET INCREMENTED
 # DATE TO START is the oldest un-collected date; 
 # the script will go from there through yesterday in order
+#
 # example usage:
-# > perl 1getPerl6IRCLogs.pl 51255 20150406
+# > python 1getPerl6IRCLogs.py 62938 20150603 password
 #
 # purpose: 
 # grab all the IRC logs from http://irclog.perlgeek.de/perl6/
-# parse these files looking for facts to populate the django irc table
+# parse these files looking for facts to populate the perl6 irc table
+################################################################
 try:
     import urllib.request as urllib2
 except ImportError:
@@ -45,66 +48,75 @@ import sys
 import codecs
 import os
 
+datasource_id = str(sys.argv[1])
+date_to_start = str(sys.argv[2])
+password      = str(sys.argv[3])
+urlstem       = "http://irclog.perlgeek.de/perl6/"
+forge_id      = 65
+newDS         = datasource_id
 
-# --------------------------------------------------
-# get the files from the web site
-# starting with the date given on the command line
-# store each URL as a local file
-# --------------------------------------------------
-def grabFiles(dbh1, dbh2, datasource_id, urlstem, date_to_start):
-    cursor= dbh1.cursor()
-    """
-    cursor2= dbh2.cursor()
-    """       
-    p_datasource_id = datasource_id
-    p_urlstem = urlstem
-    p_date_to_start = date_to_start
+if datasource_id and date_to_start and password:
+    try:
+        db1 = pymysql.connect(host='grid6.cs.elon.edu',
+                                  database='ossmole_merged',
+                                  user='megan',
+                                  password=password,
+                                  use_unicode=True,
+                                  charset='utf8')
     
-    newds = p_datasource_id
-        
-    #get yesterday's date
-    yesterday= datetime.datetime.now()-timedelta(days= 1)
-    print ("yesterday's date is:",yesterday)
-    dates= datetime.datetime(int(p_date_to_start[0:4]),int(p_date_to_start[4:-2]),int(p_date_to_start[6:]))
-    while(dates <= yesterday):
+    except pymysql.Error as err:
+        print(err)
+    try:
+         db2 = pymysql.connect(host='flossdata.syr.edu',
+                                  database='ossmole_merged',
+                                  user='megan',
+                                  password=password,
+                                  use_unicode=True,
+                                  charset='utf8')
+    except pymysql.Error as err:
+        print(err)
+    
+    cursor1 = db1.cursor()
+    cursor2 = db2.cursor()
+    
+    os.mkdir (datasource_id)
+    
+    # get yesterday's date - we will stop collecting on that date
+    yesterday = datetime.datetime.now()-timedelta(days= 1)
+    dateS = datetime.datetime(int(p_date_to_start[0:4]),int(p_date_to_start[4:-2]),int(p_date_to_start[6:]))
+    while(dateS <= yesterday):
         print ("working on ...")
         print (date)
         
-        # get yyyy, mm, dd and put into URL
-        yyyy = dates.year
-        mm   = dates.month
+        # get yyyy, mm, dd to put into URL
+        yyyy = dateS.year
+        mm   = dateS.month
         dd   = dates.day
         # put leading zeroes on dd & mm
         if (dd < 10):
             dd = str("0" + str(dd))
         if (mm < 10):
             mm = str("0"+str(mm))
-        fileName= "/"+str(yyyy)+str(mm)+str(dd)
-        
+                
         # get file
         # Log URLs are in this format:
-        # http://irclogs.ubuntu.com/2015/03/09/%23ubuntu.txt
-        filestem = str(yyyy) + "-" +str(mm) + "-" +str(dd)
-        newURL = p_urlstem + filestem
-        print ("getting URL " + newURL)
+        # https://irclog.perlgeek.de/perl6/2016-05-20
+        newURL   = urlstem + str(yyyy) + "-" + str(mm) + "-" + str(dd)
+        print ("getting URL... ", newURL)
 
-        saveLoc = p_datasource_id +fileName
-        print ("...saving as:",  saveLoc)
+        saveLoc = datasource_id + "/" + str(yyyy) + str(mm) + str(dd)
+        print ("saving as...", saveLoc)
         
         try:
             html = urllib2.urlopen(newURL).read()
         except urllib2.HTTPError as error:
             print(error)
         else:
-            fileloc = datasource_id + fileName
-            outfile = codecs.open(fileloc,'w')
+            outfile = codecs.open(saveLoc,'w')
             outfile.write(str(html))
             outfile.close()
-        #======
-        # LOCAL
-        #======
-            try:
-                cursor.execute(u"INSERT INTO datasources1(datasource_id, \
+        
+        insertQuery = "INSERT INTO datasources(datasource_id, \
                     forge_id, \
                     friendly_name, \
                     date_donated, \
@@ -112,86 +124,49 @@ def grabFiles(dbh1, dbh2, datasource_id, urlstem, date_to_start):
                     comments, \
                     start_date, \
                     end_date)  \
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
-                    (str(newds), 
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        #======
+        # LOCAL
+        #======
+            try:
+                cursor1.execute(insertQuery, 
+                    (str(newDS), 
                      forge_id,
                      'Perl 6 IRC ' + str(yyyy) +str(mm) + str(dd),
                      datetime.datetime.now(),
-                    'msquire@elon.edu', 
-                    saveLoc, 
-                    datetime.datetime.now(), 
-                    datetime.datetime.now()))
-                dbh1.commit() 
+                     'msquire@elon.edu', 
+                     saveLoc, 
+                     datetime.datetime.now(), 
+                     datetime.datetime.now()))
+                db1.commit() 
             except pymysql.Error as error:
                 print(error)
-                dbh1.rollback()
-        #increment date by one
-        dates= dates + timedelta(days=1)
-        newds= int(newds) +1
-    cursor.close()
-    
-        #=====
+                db1.rollback()
+        #======
         # REMOTE
-        #=====
-"""
-        try:
-            cursor2.execute(u"INSERT INTO datasources(datasource_id, \
-                forge_id, \
-                friendly_name, \
-                date_donated, \
-                contact_person, \
-                comments, \
-                start_date, \
-                end_date)  \
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
-                (str(newds), 
-                forge_id,
-                'Ubunto IRC',
-                str(datetime.now()),
-                'msquire@elon.edu', 
-                fileloc, 
-                str(datetime.now()), 
-                str(datetime.now())))
-            dbh2.commit() 
-        except pymysql.Error as error:
-            print(error)
-            dbh2.rollback()
-       
-        
+        #======               
+            try:
+                cursor2.execute(insertQuery, 
+                    (str(newDS), 
+                     forge_id,
+                     'Perl 6 IRC ' + str(yyyy) +str(mm) + str(dd),
+                     datetime.datetime.now(),
+                     'msquire@elon.edu', 
+                     saveLoc, 
+                     datetime.datetime.now(), 
+                     datetime.datetime.now()))
+                db2.commit() 
+            except pymysql.Error as error:
+                print(error)
+                db2.rollback()
         #increment date by one
-        date= date + timedelta(day = 1)
-        newds= newds +1
-"""
-
-datasource_id = str(sys.argv[1])
-date_to_start = str(sys.argv[2])
-password= str(sys.argv[3])
-urlstem = "http://irclog.perlgeek.de/perl6/"
-forge_id = 65
-
-if datasource_id and date_to_start:
-    try:
-        dbh1 = pymysql.connect(host='grid6.cs.elon.edu',
-                                  database='test',
-                                  user='gbatchelor',
-                                  password=password,
-                                  charset='utf8')
+        dateS = dateS + timedelta(days=1)
+        newDS += 1
+    cursor1.close()
+    cursor2.close()
     
-    except pymysql.Error as err:
-        print(err)
-    try:
-         dbh2 = pymysql.connect(host='flossdata.syr.edu',
-                                  database='rubygems',
-                                  user='megan',
-                                  password=password,
-                                  charset='utf8')
-    except pymysql.Error as err:
-        print(err)
-        dbh2= "remote"
-    
-    os.mkdir (datasource_id)
-    grabFiles(dbh1, dbh2, datasource_id, urlstem, date_to_start)
-    dbh1.close()
+    db1.close()
+    db2.close()
+
 else:
 	print ("You need both a datasource_id and a date to start on your commandline.")
-	exit;
