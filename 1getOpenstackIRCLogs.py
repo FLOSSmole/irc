8b77f4eaa7010d5f202466638e84cf5dcdd171a1
Megan Squire <megansquire@gmail.com>
2016-06-07 15:00:38 -0400

    formatting, minor bug fixes

--- 1getOpenstackIRCLogs.py
+++ 1getOpenstackIRCLogs.py
@@ -1,8 +1,42 @@
 # -*- coding: utf-8 -*-
-# usage: python openstack_irc_fetcher.py 53029 startdate irctype password
+## This program is free software; you can redistribute it
+## and/or modify it under the same terms as Perl itself.
+## Please see the Perl Artistic License 2.0.
+## 
+## Copyright (C) 2004-2016 Megan Squire <msquire@elon.edu>
+## With code contributions from:
+## Gavan Roth
+## Becca Gazda
+## Evan Ashwell
+## 
+## We're working on this at http://flossmole.org - Come help us build 
+## an open and accessible repository for data and analyses for open
+## source projects.
+## 
+## If you use this code or data for preparing an academic paper please
+## provide a citation to:
+## 
+## Howison, J., Conklin, M., & Crowston, K. (2006). FLOSSmole: 
+## A collaborative repository for FLOSS research data and analyses. 
+## International Journal of Information Technology and Web Engineering, 
+## 1(3), 17–26.
+## 
+## and
+## 
+## FLOSSmole (2004-2016) FLOSSmole: a project to provide academic access to data 
+## and analyses of open source projects.  Available at http://flossmole.org 
+################################################################
+# usage: 
+# > python 1getOpenstackIRCLogs.py datasource_id startdate irctype password
+#
+# example:
+# > python 1getOpenstackIRCLogs.py 63707 2014-06-10 openstack password
+#
 # startdate is the next date you want to collect from in the format 2015-08-31
 # irctype can be one of: openstack, dev, infra, meeting, meeting-alt, meeting-3, dns
 # run this seven times, once for each irc type
+################################################################
+
 try:
     import urllib.request as urllib2
 except ImportError:
@@ -17,11 +51,11 @@ import pymysql
 # ds stand for the next available data source
 #the start date is the day after the last known date data was collected
 # irctype is openstack, dev, infra, meeting, meeting-alt, meeting-3, or dns
-ds = int(sys.argv[1])
-newds = int(ds)
-mystartdate = str(sys.argv[2])
-irctype = str(sys.argv[3])
-pw = str(sys.argv[4])
+ds      = sys.argv[1]
+start   = sys.argv[2]
+irctype = sys.argv[3]
+pw      = sys.argv[4]
+newds   = int(ds)
 
 if irctype == 'openstack':
     directory = 'openstack'
@@ -67,22 +101,23 @@ elif irctype == 'dns':
         os.makedirs(directory)
 else:
     print("invalid irc type, must be one of openstack, dev, infra, meeting, meeting-alt, meeting-3, or dns")
-    exit
+    
+    
 if irctype == 'openstack':
-    friendly= 'openstack' + ' IRC Logs '
+    friendly= 'openstack IRC Logs'
 else:
     friendly = 'openstack-' + directory + ' IRC Logs '
 
 # dates
-startdate = datetime.datetime.strptime(mystartdate, '%Y-%m-%d').date()
-enddate = datetime.date.today()
-dateDonated= datetime.datetime.now()
+startDate   = datetime.datetime.strptime(start, '%Y-%m-%d').date()
+endDate     = datetime.date.today()
+dateDonated = datetime.datetime.now()
 
 # databases
 db1 = pymysql.connect(host="grid6.cs.elon.edu", 
-                      user="eashwell", 
+                      user="megan", 
                       passwd=pw, 
-                      db="test", 
+                      db="ossmole_merged", 
                       use_unicode=True, 
                       charset = "utf8")
 cursor1 = db1.cursor()
@@ -94,11 +129,12 @@ db2 = pymysql.connect(host="flossdata.syr.edu",
                       charset = "utf8")
 cursor2 = db2.cursor()
 
-while(startdate != enddate):
-    print(startdate)
-    date = datetime.date.isoformat(startdate)
+while(startDate != endDate):
+    print(startDate)
+    date = datetime.date.isoformat(startDate)
     url = urlstem + date + ".log"
-    print(url)
+    print("Working on...", url)
+    
     try:
         html = urllib2.urlopen(url).read()
         fileloc = directory + '/' + date + '.txt'
@@ -106,9 +142,7 @@ while(startdate != enddate):
         out.write(str(html))
         out.close
         
-        # put new datasource_id in databases (local and remote)
-        try:
-            cursor1.execute(u"INSERT INTO datasources(datasource_id, \
+        insertQuery = "INSERT INTO datasources(datasource_id, \
                 forge_id, \
                 friendly_name, \
                 date_donated, \
@@ -116,50 +150,38 @@ while(startdate != enddate):
                 comments, \
                 start_date, \
                 end_date)  \
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
-                (str(newds), 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        dataValues = (str(newds), 
                 forge_id,
                 friendly+date,
                 str(dateDonated),
                 'msquire@elon.edu', 
                 fileloc, 
-                str(enddate), 
-                str(enddate)))
+                str(endDate), 
+                str(endDate))
+                
+        # put new datasource_id in databases (local and remote)
+        try:
+            cursor1.execute(insertQuery, dataValues)
             db1.commit() 
         except pymysql.Error as error:
             print(error)
             db1.rollback()
 
         try:
-            cursor2.execute(u"INSERT INTO datasources(datasource_id, \
-                forge_id, \
-                friendly_name, \
-                date_donated, \
-                contact_person, \
-                comments, \
-                start_date, \
-                end_date)  \
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
-                (str(newds), 
-                forge_id,
-                friendly+date,
-                str(dateDonated),
-                'msquire@elon.edu', 
-                fileloc, 
-                str(enddate), 
-                str(enddate)))
+            cursor2.execute(insertQuery, dataValues)
             db2.commit() 
         except pymysql.Error as error:
             print(error)
             db2.rollback()
 
         # increment for the next file
-        startdate = startdate + datetime.timedelta(days=1)
+        startDate = startDate + datetime.timedelta(days=1)
         newds += 1
         
     except urllib2.HTTPError:
         # increment for the next file
-        startdate = startdate + datetime.timedelta(days=1)
+        startDate = startDate + datetime.timedelta(days=1)
         pass
 
 db1.close()
