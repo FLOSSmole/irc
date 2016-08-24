85c47bd906eefaf1673b4477427ef7e4ccdf2b46
Megan Squire <megansquire@gmail.com>
2016-08-24 13:46:34 -0400

    syntax & local/remote fixes

--- parseChatzillaLogFile.py
+++ parseChatzillaLogFile.py
@@ -1,9 +1,6 @@
-#!/usr/bin/perl
+# -*- coding: utf-8 -*-
 # This program is free software; you can redistribute it
-# and/or modify it under the same terms as Perl itself.
-# Please see the Perl Artistic License.
-#
-# Copyright (C) 2004-2014 Megan Squire <msquire@elon.edu>
+# and/or modify it under the GPLv2
 #
 # We're working on this at http://flossmole.org - Come help us build
 # an open and accessible repository for data and analyses for open
@@ -19,33 +16,19 @@
 #
 # and
 #
-# FLOSSmole (2004-2014) FLOSSmole: a project to provide
-# academic access to data
-# and analyses of open source projects.  Available at http://flossmole.org
+# FLOSSmole: a project to provide academic access to data
+# and analyses of open source projects. Available at http://flossmole.org
 #
 ################################################################
 # usage:
-# > perl parseChatzillaTopicList.pl <datasource_id> <file> <mode>
-#
+# > python parseChatzillaLogFile.py <datasource_id> <file> <debug mode> <password>
 #
 # example usage:
-# > perl parseChatzillaTopicList.pl 8441 log.txt PROD
+# > python parseChatzillaLogFile.py 8441 log.txt PROD password
 #
 # purpose:
-# open a chatzilla log file with /list in it, and parse out the
+# open a chatzilla log file with results of /list in it, and parse out the
 # topics, channels, and number of users; put these in table in db
-#
-# REQUIRES:
-#
-# dbInfo.pl is a text file in the same directory as this file
-# It should include your database connection info,
-# Including username, password, and dsn in the following format:
-# host
-# port
-# username
-# password
-# database
-#
 ################################################################
 import pymysql
 import re
@@ -59,40 +42,25 @@ DEBUG = str(sys.argv[3])
 password = str(sys.argv[4])
 
 if (DEBUG and datasource_id and file):
-
-    # connect to db
-
-    if (DEBUG is "DEBUG"):
-
-        DBFILE = "dbInfoTest.txt"
-        print("\n" + DEBUG + "run \n")
-    else:
-        DBFILE = "dbInfo.txt"
-        print("\nPRODUCTION run\n")
-
-    try:
-        dbh1 = pymysql.connect(host='grid6.cs.elon.edu',
-                               database='test',
-                               user='eashwell',
-                               password=password,
-                               charset='utf8')
-    except pymysql.Error as err:
-        print(err)
-    cursor1 = dbh1.cursor()
-    """
-    # =======
-    # REMOTE
-    # =======
-    try:
-        dbh2 = pymysql.connect(host='flossdata.syr.edu',
-                               database='bitcoin',
-                               user='megan',
-                               password=password,
-                               charset='utf8')
-    except pymysql.Error as err:
-        print(err)
-    cursor2 = dbh2.cursor()
-    """
+    if DEBUG == 'PROD':
+        try:
+            dbh1 = pymysql.connect(host='grid6.cs.elon.edu',
+                                   database='irc',
+                                   user='megan',
+                                   password=password,
+                                   charset='utf8')
+        except pymysql.Error as err:
+            print(err)
+        cursor1 = dbh1.cursor()
+        try:
+            dbh2 = pymysql.connect(host='flossdata.syr.edu',
+                                   database='irc',
+                                   user='megan',
+                                   password=password,
+                                   charset='utf8')
+        except pymysql.Error as err:
+            print(err)
+        cursor2 = dbh2.cursor()
 
     # read in file
     print("opening file:" + file)
@@ -107,7 +75,6 @@ if (DEBUG and datasource_id and file):
     # https://softuni.bg/ - Software University Bulgaria
 
     for line in log:
-
         # parse out channel name
         line = re.search('^(.*?)===\s#(.*?)\s+(.*?)\s+(.*?)$', str(line))
         if line:
@@ -130,11 +97,11 @@ if (DEBUG and datasource_id and file):
         if ((DEBUG == "PROD")):
             print("ran")
             try:
-                insertQuery = "INSERT INTO `fn_irc_channels`(`channel_name`,\
-                                                         `num_users`,\
-                                                         `topic`,\
-                                                         `datasource_id`,\
-                                                         `last_updated`)\
+                insertQuery = "INSERT INTO fn_irc_channels (channel_name,\
+                                                         num_users,\
+                                                         topic,\
+                                                         datasource_id,\
+                                                         last_updated)\
                                                          VALUES (%s,%s,%s,%s,\
                                                          %s)"
                 cursor1.execute(insertQuery, (channel_name, num_users,
@@ -146,10 +113,12 @@ if (DEBUG and datasource_id and file):
                 dbh1.rollback()
 
     cursor1.close()
-
     dbh1.close()
+    cursor2.close()
+    dbh2.close()
 
 else:
     print("You need a datasource_id, a file,\
           and a debug mode on your commandline.")
     exit
+    
